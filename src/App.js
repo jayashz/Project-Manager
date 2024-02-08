652e6cc63816440edda3afab3a98e46867be4f40
jayashz <jayzstha15@gmail.com>
2024-02-08 23:17:38 +0545

    the added tasks features on the selected projects

--- src/App.js
+++ src/App.js
@@ -9,8 +9,26 @@ function App() {
   const [projectState, setProjectState] = useState({
     selectedProjectId: undefined,
     project: [],
+    tasks: [],
   });
 
+  function handleAddTasks(text) {
+    setProjectState((prevState) => {
+      const taskId=Math.random();
+      const newTask = {
+        text:text,
+        projectId:prevState.selectedProjectId,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        task:[newTask, ...prevState.tasks]
+      };
+    });
+  };
+  function handleDeleteTasks() {
+
+  };
   const handlerSelectedProject = (id) => {
     setProjectState((prevState) => {
       return {
@@ -49,9 +67,32 @@ function App() {
       };
     });
   };
-  const selectedProject = projectState.project.find(project=> project.id===projectState.selectedProjectId);
 
-  let content =<SelectedProject project={selectedProject} />;
+  const handleDelete = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        project: prevState.project.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  };
+
+  const selectedProject = projectState.project.find(
+    (project) => project.id === projectState.selectedProjectId
+  );
+
+  let content = (
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDelete}
+      onAddTask={handleAddTasks}
+      onDeleteTasks={handleDeleteTasks}
+      tasks={projectState.tasks}
+    />
+  );
 
   if (projectState.selectedProjectId === null) {
     content = (
@@ -60,7 +101,6 @@ function App() {
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoNewProject onAddProject={handleStartAddProject} />;
   }
-
 
   return (
     <main className="h-screen my-8 flex gap-8">
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import NewTask from "./NewTask";
+
+const Tasks = ({ tasks, onAdd, onDelete }) => {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <NewTask onAdd={onAdd} />
+      {tasks.length === 0 && (
+        <p className="text-stone-800 mb-4 my-4">
+          This project has no tasks yet!
+        </p>
+      )}
+      {tasks.length > 0 && (
+        <ul className="p-4 mb-7 rounded-sm bg-stone-100">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <button className="text-stone-700 hover:text-red-500">Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <ul></ul>
+    </section>
+  );
+};
+
+export default Tasks;
